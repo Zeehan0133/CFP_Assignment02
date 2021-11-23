72bfc0cc5829df56d8a2b9255f6caecdff3feaca
Zeehan0133 <zyne0133@gmail.com>
2021-11-23 22:58:38 +0530

    [Zeehan]Added(check Employee attendance)

---
+++ Employee_Attendance.py
@@ -0,0 +1,8 @@
+import random
+"""Check whether a employee is present or not """
+Full_Time=1
+empCheck = random.randint(0, 2)
+if empCheck==Full_Time:
+    print("Employee is Present ")
+else:
+    print("Employee is Absent ")
