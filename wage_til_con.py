5ed60c95c63d552fb2bfe1eb92ee047bbf3106d8
Zeehan0133 <zyne0133@gmail.com>
2021-11-23 23:28:28 +0530

    [Zeehan]Added(Calculate Wages til a condition of total working hours or days is reached for a month)

---
+++ wage_til_con.py
@@ -0,0 +1,26 @@
+import random
+"""Calculate Wages til a condition of total working hours or days is reached for a month"""
+IS_PART_TIME = 1
+IS_FULL_TIME = 2
+EMP_RATE_PER_HOUR = 20
+NUM_OF_WORKING_DAYS = 2
+MAX_HRS_IN_MONTH = 10
+def CalculateWageTill100HrsOr20IsReached():
+    empHrs = 0
+    totalEmpHrs = 0
+    totalWorkingDays = 0
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS):
+        totalWorkingDays+=1
+        empCheck = random.randint(0, 3)
+        if (empCheck==IS_PART_TIME):
+            empHrs = 4
+        elif(empCheck== IS_FULL_TIME):
+            empHrs = 8
+        else: 
+            empHrs = 0 
+        totalEmpHrs += empHrs
+        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR
+        print(totalEmpWage)
+
+if __name__=='__main__': 
+    CalculateWageTill100HrsOr20IsReached()
